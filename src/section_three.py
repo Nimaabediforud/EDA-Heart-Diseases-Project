d0a5e82b0723b33920f25208d4f08f906e793637
nima abedi forud <nimaa9704@gmail.com>
2025-07-10 23:37:24 +0330

    update project

--- src/section_three.py
+++ src/section_three.py
@@ -1,6 +1,8 @@
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
+from matplotlib.gridspec import GridSpec
+
 
 
 
@@ -45,7 +47,7 @@ def stacked_bar(ax, xticks, xticks_labels, patient_counts, non_patient_counts):
     ax.bar(xticks, patient_counts, width=0.4, bottom=non_patient_counts, label="Patients",edgecolor="black", color="lightcoral")
 
     # Customize the plot and figure
-    ax.set_title("Distribution of Patients & Non-Patients in Each Chest Pain Type", fontdict=title)
+    ax.set_title("Distribution of Chest Pain Types in Patients and Non-Patients", fontdict=title)
     ax.set_xlabel("Chest Pain Types", fontdict=label)
     ax.set_ylabel("Population", fontdict=label)
     ax.grid(axis="y")
@@ -98,11 +100,79 @@ def stacked_bar(ax, xticks, xticks_labels, patient_counts, non_patient_counts):
 
 
 
-fig, axs= plt.subplots(2, 1, figsize=[10, 10])
-stacked_bar(axs[0], xticks, xticks_labels, patient_counts, non_patient_counts)
-plt.tight_layout()
+def donutChart1(axs2, patients_counts,xticks_labels, label):
+    # Set colors and donut chart
+    colors = plt.get_cmap("Set3")(np.arange(len(xticks_labels)))
+    patch1, text1, autotext1 = axs2.pie(patients_counts, labels=xticks_labels, colors=colors,
+                           autopct="%1.1f%%", pctdistance=0.75, wedgeprops=dict(width=0.5))
+    # Add edgecolor
+    for p1 in patch1:
+        p1.set_edgecolor("black")
+
+    # Adjust labels font and style 
+    for t1 in text1:
+        t1.set_fontname("Comic Sans MS")
+        t1.set_fontweight("bold")
+
+    for a1 in autotext1:
+        a1.set_fontname("Times New Roman")
+        a1.set_fontweight("bold")
+    
+    # Add text in the center of donut chart
+    axs2.text(0, 0, "Patients", fontdict=label, ha="center", va="center")
+
+
+def donutChart2(axs3, non_patient_counts,xticks_labels, label):
+    # Set colors and donut chart
+    colors = plt.get_cmap("Set3")(np.arange(len(xticks_labels)))
+    patch2, text2, autotext2 = axs3.pie(non_patient_counts, labels=xticks_labels, colors=colors,
+                           autopct="%1.1f%%", pctdistance=0.75, wedgeprops=dict(width=0.5))
+    
+    # Add edgecolor
+    for p2 in patch2:
+        p2.set_edgecolor("black")
+
+    # Adjust labels font and style 
+    for t2 in text2:
+        t2.set_fontname("Comic Sans MS")
+        t2.set_fontweight("bold")
+    
+    for a2 in autotext2:
+        a2.set_fontname("Times New Roman")
+        a2.set_fontweight("bold")
+    
+    # Add text in the center of donut chart
+    axs3.text(0, 0, "Non-Patients", fontdict=label, ha="center", va="center")
+
+
+def chest_p_t_df(patients, non_patients):
+    # Make the dataframe
+    patients_ = patients.reset_index(name="counts")
+    non_patients_ = non_patients.reset_index(name="counts")
+    chest_p_t_df = pd.merge(patients_, non_patients_, on="ChestPainType", suffixes=[" of Patients", " of Non-Patients"])
+    #chest_p_t_df.to_csv(...)
+    print(chest_p_t_df)
+
+
+
+
+# Make the figure and subplots
+fig = plt.figure(figsize=(12, 12)) 
+gs = GridSpec(nrows=2, ncols=2, figure=fig, hspace=0.4)
+axs1 = fig.add_subplot(gs[0, :])
+axs2 = fig.add_subplot(gs[1, 0])
+axs3 = fig.add_subplot(gs[1, 1])
+
+# Adjust and add the super title for pie charts
+sup_title = plt.suptitle(x=0.5, y=0.45, t="Distribution of Chest Pain Types in Patients and Non-Patients (%)",
+                         fontname="Comic Sans MS", fontsize=16, fontweight="bold")
+
+# Run and Show 
+stacked_bar(axs1, xticks, xticks_labels, patient_counts, non_patient_counts)
+donutChart1(axs2, patient_counts, xticks_labels, label)
+donutChart2(axs3, non_patient_counts, xticks_labels, label)
+#plt.savefig(...)
 plt.show()
 
-print(xticks_labels,"\n", xticks,"\n",
-      "\n",patients,"\n",non_patients,"\n")
+chest_p_t_df(patients, non_patients)
 
